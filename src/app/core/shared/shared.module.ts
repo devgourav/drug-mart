a043da7478f91dd7c8f217593c0b611281e2880e
Dev Gourav <devgrv@grpbazaar.com>
2019-03-20 09:51:04 +0530

    Added Export as pdf in vendor details

--- src/app/core/shared/shared.module.ts
+++ src/app/core/shared/shared.module.ts
@@ -6,12 +6,8 @@ import { QuantityDirective } from '../directive/quantity.directive';
 import { AlphabetDirective } from '../directive/alphabet.directive';
 
 @NgModule({
-  declarations: [SharedComponent,CurrencyDirective,QuantityDirective,AlphabetDirective],
-  imports: [
-    CommonModule
-  ],
-  exports:[
-    CurrencyDirective,QuantityDirective,AlphabetDirective
-  ]
+	declarations: [ SharedComponent, CurrencyDirective, QuantityDirective, AlphabetDirective ],
+	imports: [ CommonModule ],
+	exports: [ CurrencyDirective, QuantityDirective, AlphabetDirective ]
 })
-export class SharedModule { }
+export class SharedModule {}
--- src/app/modules/vendor/vendor-details/vendor-details.component.ts
+++ src/app/modules/vendor/vendor-details/vendor-details.component.ts
@@ -1,54 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { Vendor } from 'src/app/core/model/vendor.model';
 import { VendorService } from 'src/app/core/service/vendor.service';
 import { Subscription } from 'rxjs';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
-
-const confirmMsg = "Do you want to delete this vendor?";
-
+const confirmMsg = 'Do you want to delete this vendor?';
 
 @Component({
-  selector: 'app-vendor-details',
-  templateUrl: './vendor-details.component.html',
-  styleUrls: ['./vendor-details.component.scss']
+	selector: 'app-vendor-details',
+	templateUrl: './vendor-details.component.html',
+	styleUrls: [ './vendor-details.component.scss' ]
 })
 export class VendorDetailsComponent implements OnInit {
-  vendors: Vendor[] = [];
-  private subscriptions: Array<Subscription> = [];
+	vendors: Vendor[] = [];
+	private subscriptions: Array<Subscription> = [];
 
-  constructor(private _vendorService: VendorService,
-    private router: Router) {
-  }
+	constructor(private _vendorService: VendorService, private router: Router) {}
 
-  vendorDetailsTableHeaders = ['Vendor Name','Contact Name','Address',
-  'Email','Vendor Phone','Contact Phone','Actions']
+	vendorDetailsTableHeaders = [
+		'Vendor Name',
+		'Contact Name',
+		'Address',
+		'Email',
+		'Vendor Phone',
+		'Contact Phone',
+		'Actions'
+	];
 
-  ngOnInit() {
-    this.getVendors();
-  }
+	exportVendorDetailsTableHeaders = [
+		'Vendor Name',
+		'Contact Name',
+		'Address',
+		'Email',
+		'Vendor Phone',
+		'Contact Phone'
+	];
 
-  getVendors(){
-    this.subscriptions.push(this._vendorService.getVendors()
-    .subscribe((response)=>{
-      this.vendors = response;
-    }))
-  }
+	ngOnInit() {
+		this.getVendors();
+	}
 
-  deleteVendor(vendor: Vendor){
-    if(confirm(confirmMsg)){
-      this._vendorService.deleteVendor(vendor);
-    }
-  }
+	getVendors() {
+		this.subscriptions.push(
+			this._vendorService.getVendors().subscribe((response) => {
+				this.vendors = response;
+			})
+		);
+	}
 
-  editVendor(vendorId: string){
-    this.router.navigate(['Vendors/New Vendor',vendorId]);
-  }
+	deleteVendor(vendor: Vendor) {
+		if (confirm(confirmMsg)) {
+			this._vendorService.deleteVendor(vendor);
+		}
+	}
 
-  ngOnDestroy() {
-    this.subscriptions.forEach((subscription: Subscription) => {
-      subscription.unsubscribe();
-    });
-  }
+	editVendor(vendorId: string) {
+		this.router.navigate([ 'Vendors/New Vendor', vendorId ]);
+	}
 
+	ngOnDestroy() {
+		this.subscriptions.forEach((subscription: Subscription) => {
+			subscription.unsubscribe();
+		});
+	}
+
+	@ViewChild('screen') screen: ElementRef;
+	saveAsPDF() {
+		let pdf = new jsPDF('portrait', 'mm', 'a4');
+		pdf.setProperties({
+			title: 'Pdf Export',
+			subject: 'This is the subject',
+			author: 'Kanchan Medico',
+			keywords: 'generated, javascript, web 2.0, ajax',
+			creator: 'Drug Mart'
+		});
+
+		html2canvas(this.screen.nativeElement).then((canvas) => {
+			var imgData = canvas.toDataURL('image/png');
+			var imgWidth = 208;
+			var pageHeight = 295;
+			var imgHeight = canvas.height * imgWidth / canvas.width;
+
+			pdf.addImage(imgData, 0, 0, imgWidth, imgHeight);
+			pdf.save('test.pdf');
+		});
+	}
 }
